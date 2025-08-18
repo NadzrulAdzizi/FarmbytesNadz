0d2d75bad05b6dddcde2a1371badc9a5871d068d
Adlan Elias <adlan.hanif@farmbyte.com>
2025-08-18 15:26:37 +0800

    Demo Auto FBG 18/08/2025

---
+++ testNadz/DemoAuto_FBG/playwright.config.js
@@ -0,0 +1,15 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  use: {
+    headless: false,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'], headless: false },
+    },
+  ],
+});
